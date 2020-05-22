af5cefaba43f1ebc4e8694921abfc21a91b08baa
eeepad86 <francescoauriemama@gmail.com>
2020-05-22 17:47:25 +0200

    refactor main module

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from utils import *
 
 logging.basicConfig(level=logging.DEBUG)
 
-IMAGE_NAME = "frag_2.png"
+IMAGE_NAME = "frag_56.png"
 
 
 def get_img_path(image_name):
@@ -41,96 +41,55 @@ def verify_if_path_exists(path):
     """
     return os.path.exists(path)
 
+def expand_image_first_solution(image,iterations):
+    RIGHT_COLUMN_BOUND = get_image_width(fragment_image) - 1
+    BOTTOM_ROW_BOUND = get_image_height(fragment_image) - 1
 
-def expand_image(image,iterations):
-    LEFT_COLUMN_BOUND = 1
-    RIGHT_COLUMN_BOUND = get_image_width(image) - 1
+    IMAGE_CENTER_X = int(get_image_width(fragment_image) / 2)
+    IMAGE_CENTER_Y = int(get_image_height(fragment_image) / 2)
 
-    TOP_ROW_BOUND = 1
-    BOTTOM_ROW_BOUND = get_image_height(image) - 1
+    START_ROW_VALUE = 1
+    START_COLUMN_VALUE = 1
 
-    IMAGE_CENTER_X = int(get_image_width(image) / 2)
-    IMAGE_CENTER_Y = int(get_image_height(image) / 2)
-
-    # SUGGESTED VALUES ARE 25 AND 80
-    MINIMUM_PERCENTAGE_OF_BLACK = 30
-    MAXIMUM_PERCETANGE_OF_BLACK = 70
-    logging.info("RIGHT_COLUMN_BOUND: {}. BOTTOM_ROW_BOUND: {}".format(RIGHT_COLUMN_BOUND, BOTTOM_ROW_BOUND))
-
+    # FIRST  CASE: FIRST  QUADRANT 1-1
+    # SECOND CASE: SECOND QUADRANT 1-110 RIGHT-COLUMN-BOUND
+    # THIRD  CASE: THIRD  QUADRANT 110-1 BOTTOM-ROW-BOUND
+    # FOURTH CASE: FOURTH QUADRANT 110-110 RIGHT-COLUMN-BOUND BOTTOM-ROW-BOUND
     for i in range(1,iterations):
-
-        #   FIRST FOR LOOP
-        #   Scan the image pixel by pixel.
-        #   First scan all the rows from the top to the middle of the image.
-        #   After all the columns from the right to the center of the image
-
-        for row in range(TOP_ROW_BOUND, IMAGE_CENTER_X):
-            for column in range(RIGHT_COLUMN_BOUND, IMAGE_CENTER_Y):
-                # I want to overwrite only black pixels.
-                # In this case the program will not do unnecessary operations.
-                if (is_image_pixel_black(image, row, column)):
-                    black_neigh_percent = get_percentage_of_black_neighbors(image, row, column)
-                    #
-                    # High percentages of black pixels indicate the background
-                    # High percentages of other colors indicate the center of the fragment
-                    #
-                    # In this case I'm on the border.
-                    if (black_neigh_percent > MINIMUM_PERCENTAGE_OF_BLACK) and (
-                            black_neigh_percent < MAXIMUM_PERCETANGE_OF_BLACK):
-                        neighbors_color_values = get_neighbors_pixels_colors(image, row, column)
-                        for neighbor_value in neighbors_color_values:
-                            if (is_pixel_black(neighbor_value)):
-                                # It's a bad idea to overwrite a black pixel with another black pixel.
-                                pass
-                            else:
-                                # Choose the last of the neighbours
-                                image = change_pixel_color_and_return_image_BGR(image, row, column,
-                                                                                get_blue_value_of_a_pixel(neighbor_value),
-                                                                                get_green_value_of_a_pixel(neighbor_value),
-                                                                                get_red_value_of_a_pixel(neighbor_value))
-
-
-        #
-        # Read comments for previous for loop
-        #
-        for row in range(BOTTOM_ROW_BOUND, IMAGE_CENTER_X):
-            for column in range(LEFT_COLUMN_BOUND, IMAGE_CENTER_Y):
-                if (is_image_pixel_black(image, row, column)):
-                    black_neigh_percent = get_percentage_of_black_neighbors(image, row, column)
-                    if (black_neigh_percent > MINIMUM_PERCENTAGE_OF_BLACK) and (
-                            black_neigh_percent < MAXIMUM_PERCETANGE_OF_BLACK):
-                        neighbors_color_values = get_neighbors_pixels_colors(image, row, column)
-                        for neighbor_value in neighbors_color_values:
-                            if (is_pixel_black(neighbor_value)):
-                                pass
-                            else:
-                                image = change_pixel_color_and_return_image_BGR(image, row, column,
-                                                                                get_blue_value_of_a_pixel(neighbor_value),
-                                                                                get_green_value_of_a_pixel(neighbor_value),
-                                                                                get_red_value_of_a_pixel(neighbor_value))
-
-
-        #
-        # Read comments for previous for loop
-        #
-        for row in range(BOTTOM_ROW_BOUND, IMAGE_CENTER_X):
-            for column in range(RIGHT_COLUMN_BOUND, IMAGE_CENTER_Y):
-                if (is_image_pixel_black(image, row, column)):
-                    black_neigh_percent = get_percentage_of_black_neighbors(image, row, column)
-                    if (black_neigh_percent > MINIMUM_PERCENTAGE_OF_BLACK) and (
-                            black_neigh_percent < MAXIMUM_PERCETANGE_OF_BLACK):
-                        neighbors_color_values = get_neighbors_pixels_colors(image, row, column)
-                        for neighbor_value in neighbors_color_values:
-                            if (is_pixel_black(neighbor_value)):
-                                pass
-                            else:
-                                image = change_pixel_color_and_return_image_BGR(image, row, column,
-                                                                                get_blue_value_of_a_pixel(neighbor_value),
-                                                                                get_green_value_of_a_pixel(neighbor_value),
-                                                                                get_red_value_of_a_pixel(neighbor_value))
+        image = expand(image, IMAGE_CENTER_X, IMAGE_CENTER_Y, START_ROW_VALUE, START_COLUMN_VALUE)
+        image = expand(image, IMAGE_CENTER_X, IMAGE_CENTER_Y, START_ROW_VALUE, RIGHT_COLUMN_BOUND)
+        image = expand(image, IMAGE_CENTER_X, IMAGE_CENTER_Y, BOTTOM_ROW_BOUND, START_COLUMN_VALUE)
+        image = expand(image, IMAGE_CENTER_X, IMAGE_CENTER_Y, BOTTOM_ROW_BOUND, RIGHT_COLUMN_BOUND, )
 
     return image
-    # End of the function
+
+
+def expand(image,image_center_x,image_center_y,start_row_value,start_column_value):
+
+    # SUGGESTED VALUES ARE 25 AND 80
+    MINIMUM_PERCENTAGE_OF_BLACK = 25
+    MAXIMUM_PERCETANGE_OF_BLACK = 80
+
+    for row in range(start_row_value, image_center_x):
+        for column in range(start_column_value, image_center_y):
+            if (is_image_pixel_black(image, row, column)):
+                black_neigh_percent = get_percentage_of_black_neighbors(image, row, column)
+                if (black_neigh_percent > MINIMUM_PERCENTAGE_OF_BLACK) and (
+                        black_neigh_percent < MAXIMUM_PERCETANGE_OF_BLACK):
+                    neighbors_color_values = get_neighbors_pixels_colors(image, row, column)
+                    for neighbor_value in neighbors_color_values:
+                        if (is_pixel_black(neighbor_value)):
+                            # It's a bad idea to overwrite a black pixel with another black pixel.
+                            pass
+                        else:
+                            # Choose the last of the neighbours
+                            image = change_pixel_color_and_return_image_BGR(image, row, column,
+                                                                            get_blue_value_of_a_pixel(neighbor_value),
+                                                                            get_green_value_of_a_pixel(neighbor_value),
+                                                                            get_red_value_of_a_pixel(neighbor_value))
+                            
+    return image
+
 
 
 def save_image(image, filename="output_img.png"):
@@ -152,11 +111,12 @@ def save_image(image, filename="output_img.png"):
 
 
 if __name__ == "__main__":
+
     fragment_image = cv2.imread(get_img_path(IMAGE_NAME), cv2.IMREAD_COLOR)
 
-    fragment_image = expand_image(fragment_image, 20)
 
-    save_image(fragment_image, "result.png")
+    fragment_image = expand_image_first_solution(fragment_image,20)
+    save_image(fragment_image, "OUT_" + IMAGE_NAME)
 
     cv2.imshow('image', fragment_image)
     cv2.waitKey(0)
