03032b8da2012591e92952869b9a97e1dffb00b1
eeepad86 <francescoauriemama@gmail.com>
2020-05-26 19:46:55 +0200

    metodi per creare,scrivere,leggere da un file excel ( in cui verranno scritti e letti i pesi di ogni pixel)

---
+++ excel.py
@@ -0,0 +1,51 @@
+from openpyxl import load_workbook
+import xlsxwriter
+import os
+
+
+
+
+
+
+
+def createExcel(filename):
+    """
+        Get the name of the chosen fragment for create file Excel.
+
+        :param image_name:
+        :return: A string that represents the excel path.
+    """
+    folder_path = os.getcwd()
+    workbook = xlsxwriter.Workbook(filename)
+
+    os.chdir(folder_path + "\FileExcel")
+
+    workbook.close()
+    return folder_path + "\FileExcel" + "\\" + filename
+
+def writeExcel(path,value,filename):
+    """
+            :What does he do? open an excel file, write the value in a cell, save the excel file
+            :param path_excel_file, value_to_write, filename_file_excel:
+            :return: void
+    """
+    workbook = load_workbook(path)
+    sheet = workbook.active
+
+    sheet.cell(row=2, column=2).value = value
+    workbook.save(filename)
+
+def readExcel(path,row,column):
+    workbook = load_workbook(filename=path)
+    sheet = workbook["Sheet1"]
+    return sheet.cell(row,column).value
+
+
+
+
+path = createExcel("Hello.xlsx")
+writeExcel(path,2,"Hello.xlsx")
+
+value=readExcel(path,2,2)
+print(value)
+
